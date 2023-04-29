59c3cbc1639171299ef05f4ad945bc99e9dbcebd
ZeusSec1337 <104400603+ZeusSec1337@users.noreply.github.com>
2023-04-29 12:20:03 +0700

    Update start.bat

--- resources/start.bat
+++ resources/start.bat
@@ -1,22 +1,22 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By its_star_boi" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By zeussec1337_at_sanggam" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user its_star_boi Admin@null /add >nul
-net localgroup administrators its_star_boi /add >nul
-net user its_star_boi /active:yes >nul
+net user sanggam_sidabutar Admin@null /add >nul
+net localgroup administrators sanggam_sidabutar /add >nul
+net user sanggam_sidabutar /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant its_star_boi:F >nul
-ICACLS C:\Windows\installer /grant its_star_boi:F >nul
+ICACLS C:\Windows\Temp /grant sanggam_sidabutar:F >nul
+ICACLS C:\Windows\installer /grant sanggam_sidabutar:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: its_star_boi
+echo Username: sanggam_sidabutar
 echo Password: Admin@null
 echo You can login now
-echo RDP By @its_star_boi
+echo RDP By @zeussec1337
 ping -n 10 127.0.0.1 >nul
